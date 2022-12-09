a46f94e1651054cb18c814cfedf8e0fb5103e877
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-12-09 13:57:25 +0300

    commit tree a2d1b8d5396aeba9732a5978370e95d44df6ded6

--- src/features/astronauts/astronautsSlice.js
+++ src/features/astronauts/astronautsSlice.js
@@ -1,21 +1,35 @@
 // Action Creators
-export function fetchAstronauts(astronauts) {
-  return {
-    type: "astronauts/astronautsLoaded",
-    payload: astronauts,
+export function fetchAstronauts() {
+  return function (dispatch) {
+    dispatch({ type: "astronauts/astronautsLoading" });
+    fetch("http://api.open-notify.org/astros.json")
+      .then((response) => response.json())
+      .then((astronauts) =>
+        dispatch({
+          type: "astronauts/astronautsLoaded",
+          payload: astronauts.people,
+        })
+      );
   };
 }
 
 // Reducers
 const initialState = {
   entities: [], //array of astronauts
+  status: "idle",
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case "astronauts/astronautsLoading":
+      return {
+        ...state,
+        status: "loading",
+      };
     case "astronauts/astronautsLoaded":
       return {
         ...state,
+        status: "idle",
         entities: action.payload,
       };
 
